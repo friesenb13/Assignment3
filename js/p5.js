6ce12a59bf72dfb3b7896b42ab813beed8137188
Becky <77301055+friesenb13@users.noreply.github.com>
2021-04-08 00:15:25 -0600

    progress?

--- js/p5.js
+++ js/p5.js
@@ -1,6 +1,9 @@
 
 var vidCapture, ctracker, drawCanvas;
 
+//https://p5js.org/reference/#/p5/createSlider
+// let slider;
+let level=0;
 
 function setup(){
 	var cnv = createCanvas(windowWidth, windowHeight);
@@ -15,55 +18,79 @@ function setup(){
 	ctracker.init();
 	ctracker.start(vidCapture.elt)
 
-	//testing
+
 	drawCanvas = document.getElementById('defaultCanvas0');
+
+	// colorMode(HSB);
+	// slider = createSlider(0, 360, 60, 40);
+	// slider.position(10,10);
+	// slider.style('width', '80px');
+
+	valueSlider = createSlider(0,3,0, 1);
+	valueSlider.position(10,10);
+	valueSlider.input(changeLevel);
 
 }
 
 function draw() {
 
-	background(255, 10);
-	translate(vidCapture.width, 0)
+	// let val = slider.value();
+	// background(val, 100, 100, 10);
+	// background(255, 10);
+
+
+	translate(vidCapture.width, 0);
+
+
 	scale(-1, 1)
 	
+	
+
 	//show your face video or not 
-	// image(vidCapture, 0, 0);
+	image(vidCapture, 0, 0);
 
-	var positions = ctracker.getCurrentPosition();
+	var position = ctracker.getCurrentPosition();
 
-	// print(positions)
-	if(positions){
+	// filter(INVERT, level);
+
+	if(position){
 
 		// all the fun with positions in here
 
-		// ctracker.draw(drawCanvas);
+		ctracker.draw(drawCanvas);
 
-		//ellipse needs a x, y, diameter
-		fill(0);
+		// ellipse needs a x, y, diameter
+		// fill(0);
 
-		//draw a point for every part of your face
-		positions.forEach(function(pos){
-			ellipse(pos[0],pos[1],2)
+		// draw a point for every part of your face
+		// positions.forEach(function(pos){
+		// 	ellipse(pos[0],pos[1],2)
 
-		})
-		// ellipse(positions[62][0],positions[62][1],10)
-
-		line(positions[23][0],positions[23][1], positions[23][0]-30,positions[23][1]-30);
-		line(positions[28][0],positions[28][1], positions[28][0]+30,positions[28][1]-30);
-
-		print("x:" + positions[62][0])
-		print("y:" + positions[62][1])
-
-		var r= map(positions[62][0], 250, 300, 0,255, true)
-		var b= map(positions[62][1], 150, 200, 0,255, true)
+		// })
+		// var r= map(position[62][0], 250, 300, 0,255, true)
+		// var b= map(position[62][1], 150, 200, 0,255, true)
 
 
-		background(r, 255,b);
-		tint(255, 126);
-		image(vidCapture,0,0);
+		// fill(r, 255,b);
+
+		// ellipse(position[62][0],position[62][1],100)
+
+		// line(positions[23][0],positions[23][1], positions[23][0]-30,positions[23][1]-30);
+		// line(positions[28][0],positions[28][1], positions[28][0]+30,positions[28][1]-30);
+
+		// print("x:" + position[62][0])
+		// print("y:" + position[62][1])
+
+		
+		// tint(255, 126);
+		// image(vidCapture,0,0);
 }
 	
 
+}
+
+function changeLevel(){
+	level=valueSlider.value();
 }
 
 // function windowResized() {
