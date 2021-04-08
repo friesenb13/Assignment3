3369b6bfc60492f1e920c6e66a1a94ecd12b305f
Becky <77301055+friesenb13@users.noreply.github.com>
2021-04-08 11:59:28 -0600

    narrowed down idea

--- js/p5.js
+++ js/p5.js
@@ -3,9 +3,10 @@ var vidCapture, ctracker, drawCanvas;
 
 //https://p5js.org/reference/#/p5/createSlider
 // let slider;
-let level=0;
 
 function setup(){
+
+
 	var cnv = createCanvas(windowWidth, windowHeight);
 	cnv.parent("p5canvas");
 
@@ -21,19 +22,18 @@ function setup(){
 
 	drawCanvas = document.getElementById('defaultCanvas0');
 
+// color slider background
 	// colorMode(HSB);
 	// slider = createSlider(0, 360, 60, 40);
 	// slider.position(10,10);
 	// slider.style('width', '80px');
 
-	valueSlider = createSlider(0,3,0, 1);
-	valueSlider.position(10,10);
-	valueSlider.input(changeLevel);
 
 }
 
 function draw() {
 
+//color background slider
 	// let val = slider.value();
 	// background(val, 100, 100, 10);
 	// background(255, 10);
@@ -44,54 +44,36 @@ function draw() {
 
 	scale(-1, 1)
 	
-	
-
-	//show your face video or not 
-	image(vidCapture, 0, 0);
 
 	var position = ctracker.getCurrentPosition();
 
-	// filter(INVERT, level);
 
 	if(position){
 
-		// all the fun with positions in here
+		// ctracker.draw(drawCanvas);
 
-		ctracker.draw(drawCanvas);
-
-		// ellipse needs a x, y, diameter
-		// fill(0);
-
-		// draw a point for every part of your face
-		// positions.forEach(function(pos){
-		// 	ellipse(pos[0],pos[1],2)
-
-		// })
-		// var r= map(position[62][0], 250, 300, 0,255, true)
-		// var b= map(position[62][1], 150, 200, 0,255, true)
+		// print("x: " + position[62][0])
 
 
-		// fill(r, 255,b);
+//want video feed to have invert filter when face is on right side of screen, and 
+//gray filter on left side of screen
 
-		// ellipse(position[62][0],position[62][1],100)
+	var invert= map(position[62][0], 100, 500, true)
+	var gray= map(position[62][1],501, 800, true)
 
-		// line(positions[23][0],positions[23][1], positions[23][0]-30,positions[23][1]-30);
-		// line(positions[28][0],positions[28][1], positions[28][0]+30,positions[28][1]-30);
 
-		// print("x:" + position[62][0])
-		// print("y:" + position[62][1])
+//show your face video or not 
+	image(vidCapture, 0, 0);
 
-		
-		// tint(255, 126);
-		// image(vidCapture,0,0);
+	filter(INVERT, invert);
+	filter(GRAY, gray);
+
 }
 	
 
 }
 
-function changeLevel(){
-	level=valueSlider.value();
-}
+
 
 // function windowResized() {
 //   resizeCanvas(windowWidth/2, windowHeight/2);
